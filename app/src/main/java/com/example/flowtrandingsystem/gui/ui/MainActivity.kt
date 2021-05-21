f8787cdde56eba9e7b663a70c76e2f24f8c82552
Guilherme Ferreira <guilhermefe007@hotmail.com>
2021-05-21 16:41:37 -0300

    Implementando mascara no login

--- app/src/main/java/com/example/flowtrandingsystem/gui/ui/MainActivity.kt
+++ app/src/main/java/com/example/flowtrandingsystem/gui/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.flowtrandingsystem.gui.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -14,11 +15,11 @@ class MainActivity: AppCompatActivity() {
     private lateinit var menuFragment: Fragment
     private lateinit var toolbar: Toolbar
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-
 
         supportActionBar?.hide();
 
