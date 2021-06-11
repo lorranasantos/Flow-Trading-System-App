bd95e5871cf688b2536dbd26c03b305d3cc11f97
Guilherme Ferreira <guilhermefe007@hotmail.com>
2021-06-11 19:49:00 -0300

    Refatoração e merge na develop

--- app/src/main/java/com/example/flowtrandingsystem/gui/ui/PdvActivity.kt
+++ app/src/main/java/com/example/flowtrandingsystem/gui/ui/PdvActivity.kt
@@ -1,29 +1,23 @@
 package com.example.flowtrandingsystem.gui.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-<<<<<<< HEAD
-import com.example.flowtrandingsystem.R
-=======
-import androidx.recyclerview.widget.RecyclerView
 import com.example.flowtrandingsystem.R
 import com.example.flowtrandingsystem.gui.adapter.BarCodeAdapter
 import com.example.flowtrandingsystem.gui.api.ProductCalls
 import com.example.flowtrandingsystem.gui.api.RetrofitApi
->>>>>>> 23b1629ba906d9f3cbb9c49fa4b3caecc8e36a85
 import com.example.flowtrandingsystem.gui.http.HttpHelper
 import com.example.flowtrandingsystem.gui.model.Product
 import com.example.flowtrandingsystem.gui.model.RegisterClientPdv
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.client_register_pdv.*
-import kotlinx.android.synthetic.main.code_scanner_activity.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class PdvActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -43,17 +37,13 @@ class PdvActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
 
-        setContentView(R.layout.pdv_activity)
-=======
         setContentView(R.layout.pdv)
 
         doAsync {
             val http = HttpHelper()
             http.getProduct()
         }
->>>>>>> 23b1629ba906d9f3cbb9c49fa4b3caecc8e36a85
 
         buttonAdicionarCliente = findViewById(R.id.pdv_client_register)
         buttonAdicionarCliente.setOnClickListener(this)
@@ -76,33 +66,31 @@ class PdvActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-<<<<<<< HEAD
-=======
-    private fun addCode() {
-
-        val editCode = findViewById<EditText>(R.id.pdv_activity_product_code)
-
-        var itemProduct: Product
-
-        val retrofit = RetrofitApi.getRetrofit()
-        val productBarCode = retrofit.create(ProductCalls::class.java)
-
-        val call = productBarCode.getBarProduct(editCode.text.toString())
-
-        call.enqueue(object : Callback<Product> {
-
-            override fun onFailure(call: Call<Product>, t: Throwable) {
-                Toast.makeText(this@PdvActivity, "Ops! Acho que ocorreu um problema.", Toast.LENGTH_SHORT).show()
-                Log.e("ERRO_CONEXÃO", t.message.toString())
-            }
-
-            override fun onResponse(call: Call<Product>, response: Response<Product>) {
-                itemProduct = response.body()!!
-                BarCodeAdapter.updateListaProdutos(itemProduct)
-
-            }
-        })
-    }
+//    private fun addCode() {
+//
+//        val editCode = findViewById<EditText>(R.id.pdv_activity_product_code)
+//
+//        var itemProduct: Product
+//
+//        val retrofit = RetrofitApi.getRetrofit()
+//        val productBarCode = retrofit.create(ProductCalls::class.java)
+//
+//        val call = productBarCode.getBarProduct(editCode.text.toString())
+//
+//        call.enqueue(object : Callback<Product> {
+//
+//            override fun onFailure(call: Call<Product>, t: Throwable) {
+//                Toast.makeText(this@PdvActivity, "Ops! Acho que ocorreu um problema.", Toast.LENGTH_SHORT).show()
+//                Log.e("ERRO_CONEXÃO", t.message.toString())
+//            }
+//
+//            override fun onResponse(call: Call<Product>, response: Response<Product>) {
+//                itemProduct = response.body()!!
+////                BarCodeAdapter.updateListaProdutos(itemProduct)
+//
+//            }
+//        })
+//    }
 
 //    private  fun loadListaItens() {
 //
@@ -129,9 +117,6 @@ class PdvActivity : AppCompatActivity(), View.OnClickListener {
 //
 //    }
 
-
-
->>>>>>> 23b1629ba906d9f3cbb9c49fa4b3caecc8e36a85
     private fun openAddDiscount() {
         val alertDialog = AlertDialog.Builder(this)
         val view = layoutInflater.inflate(R.layout.add_discount_pdv, null)
@@ -175,3 +160,4 @@ class PdvActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 }
+
