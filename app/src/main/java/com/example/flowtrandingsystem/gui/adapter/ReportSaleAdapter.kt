3fc8f570aef13080033c90b02a2baa6edac59e8e
Andre Ferreira <guilhermefe007@hotmail.com>
2021-07-06 14:57:08 -0300

    Preparação para tela relatorios

--- app/src/main/java/com/example/flowtrandingsystem/gui/adapter/ReportSaleAdapter.kt
+++ app/src/main/java/com/example/flowtrandingsystem/gui/adapter/ReportSaleAdapter.kt
@@ -13,17 +13,11 @@ import com.example.flowtrandingsystem.gui.model.Sale
 class ReportSaleAdapter(val context: Context): RecyclerView.Adapter<ReportSaleAdapter.Holder>() {
 
     var listSale = emptyList<Sale>()
-    var listReportSale = emptyList<ReportSale>()
 
     fun updateSaleList(list: List<Sale>){
         listSale = list
         notifyDataSetChanged()
     }
-    fun updateReportSale(list: List<ReportSale>){
-        listReportSale = list
-        notifyDataSetChanged()
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater
             .from(context)
@@ -38,12 +32,10 @@ class ReportSaleAdapter(val context: Context): RecyclerView.Adapter<ReportSaleAd
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val recentSale = listSale[position]
-        val recentReport = listReportSale[position]
 
 
-        holder.itemName.text = recentReport.product_name
-        holder.itemQuantity.text = recentSale.items?.get(position)?.quantity.toString()
-
+        holder.itemName.text = recentSale.items?.get(position)?.product_id.toString()
+        holder.itemQuantity.text = recentSale.items?.get(position)?.product_id.toString()
 
     }
 
